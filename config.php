fe3f9c254f3bcfbf991ee2aa2eddd549d019f792
vcspc <vitonilcius@gmail.com>
2023-05-24 15:18:48 -0300

    Assunto: Conexão com o Banco de Dados Descrição: - Criação do arquivo de configuração com o banco de dados

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'admin';
+    $dbName = 'cultoDeSabado';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    if($conexao->connect_errno){
+        echo "Erro";
+    } else {
+        echo "Conexão efetuada com sucesso";
+    }
+?>
